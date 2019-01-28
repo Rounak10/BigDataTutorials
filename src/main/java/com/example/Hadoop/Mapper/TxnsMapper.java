8dfa8db195bb958208383c61c703b85c4de6c2d6
Rounak10 <rounakdigambar22@gmail.com>
2019-01-28 19:19:00 +0530

    Add ReduceSideJoin Program

---
+++ src/main/java/com/example/Hadoop/Mapper/TxnsMapper.java
@@ -0,0 +1,23 @@
+package com.example.Hadoop;
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+/* Mapper class for Transaction Data */
+public static class TxnsMapper extends Mapper <Object, Text, Text, Text>
+    {
+        public void map(Object key, Text value, Context context)
+            throws IOException, InterruptedException
+        {
+            String record = value.toString();
+            String[] parts = record.split(",");
+            context.write(new Text(parts[2]), new Text("tnxn\t" + parts[3]));
+        }
+    }
